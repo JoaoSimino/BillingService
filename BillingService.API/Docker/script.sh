8ce8d25eae7314141ba77c9f289a7c42e1dd2da0
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-11 09:03:06 -0300

    Adicionando pasta do Docker com comando para subir o rabbit, para testes em desenvolvimento!

---
+++ BillingService.API/Docker/script.sh
@@ -0,0 +1,2 @@
+﻿#comando para subir o docker, no Windows o docker.desktop deve estar rodando (para validacoes do ambiente de microservicos utilizar tudo em containers!)
+#docker run -d --hostname rabbit --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management

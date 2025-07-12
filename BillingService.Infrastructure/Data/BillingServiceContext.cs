9f1532a0fb56423489348bc87de1c7147f879afb
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-12 18:26:14 -0300

    criacao da estrutura com entity framework para armazenamento do projeto.

---
+++ BillingService.Infrastructure/Data/BillingServiceContext.cs
@@ -0,0 +1,14 @@
+﻿using BillingService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillingService.Infrastructure.Data;
+
+public class BillingServiceContext : DbContext
+{
+    public BillingServiceContext(DbContextOptions<BillingServiceContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<PropostaAprovadaEvent> PropostasAprovadaEvento { get; set; }
+}
---
+++ BillingService.Infrastructure/Migrations/20250712212332_FirstMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BillingService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PropostasAprovadaEvento",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PropostaId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClienteId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ValorAprovado = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataRecebimento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropostasAprovadaEvento", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PropostasAprovadaEvento");
+        }
+    }
+}

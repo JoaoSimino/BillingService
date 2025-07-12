34b101d44c9d1e51e1bcf4aea923153e3965e5ac
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-12 19:02:36 -0300

    criando estrutura de servicos no projeto application para utilizar no container di posteriormente!

---
+++ BillingService.Application/Services/ICrudService.cs
@@ -0,0 +1,12 @@
+﻿namespace BillingService.Application.Services;
+
+public interface ICrudService<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(object id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+
+}
+
---
+++ BillingService.Application/Services/PropostaAprovadaEventService.cs
@@ -0,0 +1,13 @@
+﻿using BillingService.Domain.Entities;
+using BillingService.Infrastructure.Data;
+
+namespace BillingService.Application.Services;
+
+public class PropostaAprovadaEventService : CrudService<PropostaAprovadaEvent>, IPropostaAprovadaEventService
+{
+    public PropostaAprovadaEventService(BillingServiceContext context) : base(context)
+    {     
+    }
+}
+
+

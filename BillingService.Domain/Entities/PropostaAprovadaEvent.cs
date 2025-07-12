1a9bb9ea4c05869c7362be9257ff1c232202ca67
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-12 18:26:14 -0300

    criacao da estrutura com entity framework para armazenamento do projeto.

--- BillingService.Domain/Entities/PropostaAprovadaEvent.cs
+++ BillingService.Domain/Entities/PropostaAprovadaEvent.cs
@@ -2,8 +2,11 @@
 
 public class PropostaAprovadaEvent
 {
+    public Guid Id { get; set; }
     public string PropostaId { get; set; } = default!;
     public string ClienteId { get; set; } = default!;
     public decimal ValorAprovado { get; set; }
+
+    public DateTime DataRecebimento { get; set; }
 
 }
---
+++ BillingService.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BillingService.Infrastructure.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<BillingServiceContext>
+{
+    public BillingServiceContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..", "BillingService.Api"))
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var optionsBuilder = new DbContextOptionsBuilder<BillingServiceContext>();
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new BillingServiceContext(optionsBuilder.Options);
+    }
+}

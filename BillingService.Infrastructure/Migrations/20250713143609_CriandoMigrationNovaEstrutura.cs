536a5620cc139e5842272e2381dc6e8349b4b766
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-13 11:38:05 -0300

    Criando migrations da nova estrutura com as entidades de Fatura, Parcela e PropostaAprovadaEvent para atualizacao do banco de dados!

---
+++ BillingService.Infrastructure/Migrations/20250713143609_CriandoMigrationNovaEstrutura.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BillingService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoMigrationNovaEstrutura : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Faturas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PropostaAprovadaEventId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NumeroParcelas = table.Column<int>(type: "int", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Faturas_PropostasAprovadaEvento_PropostaAprovadaEventId",
+                        column: x => x.PropostaAprovadaEventId,
+                        principalTable: "PropostasAprovadaEvento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parcelas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FaturaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataVencimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataPagamento = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parcelas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Parcelas_Faturas_FaturaId",
+                        column: x => x.FaturaId,
+                        principalTable: "Faturas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Faturas_PropostaAprovadaEventId",
+                table: "Faturas",
+                column: "PropostaAprovadaEventId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parcelas_FaturaId",
+                table: "Parcelas",
+                column: "FaturaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parcelas");
+
+            migrationBuilder.DropTable(
+                name: "Faturas");
+        }
+    }
+}
